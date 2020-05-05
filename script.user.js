47b261b54c6e2577b590bf0e6e3a13f734391dfa
Lemons <24715897+Lemons1337@users.noreply.github.com>
2020-05-04 23:05:14 -0400

    Update script.user.js

--- script.user.js
+++ script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Market Item Value Calculator
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.1.1
 // @description  try to take over the world!
 // @author       Lemons
 // @match        *://krunker.io/social.html*
@@ -9,16 +9,16 @@
 // @grant        none
 // ==/UserScript==
 
-var krValue = (
-    300 / 0.99 +
-    600 / 1.79 +
-    2600 / 2.49 +
-    7000 / 15.99 +
-    20000 / 34.99 +
-    60000 / 99.99
-) / 6; // Average of all KR purchases
+const krValues = [
+    [300, 0.99],
+    [600, 1.79],
+    [2600, 2.49],
+    [7000, 15.99],
+    [20000, 34.99],
+    [60000, 99.99]
+];
 
-var fixedValue = +krValue.toFixed(2);
+const values = krValues.map(a => +(a[0] / a[1]).toFixed(2));
 
 const observer = new MutationObserver(mutations => {
     mutations.forEach(mutation => {
@@ -26,8 +26,18 @@ const observer = new MutationObserver(mutations => {
             var c = node.children;
             if (c && c[2] && c[2].className === 'marketPrice') {
                 var elem = c[2];
-                var num = elem.innerText.replace(/[^0-9]/g, '');
-                var value = (+(num / fixedValue).toFixed(2)).toLocaleString();
+
+                var num = +elem.innerText.replace(/[^0-9]/g, '');
+
+                var val = [].concat(krValues).sort((a, b) => {
+                    var a1 = num - a[0];
+                    var a2 = num - b[0];
+                    return a1 - a2;
+                });
+
+                var index = krValues.findIndex(k => k[0] === val[0][0]);
+                var value = (+(num / values[index]).toFixed(2)).toLocaleString();
+
                 node.attributes.style.value += ';height:252px';
                 node.insertAdjacentHTML('beforeend', `<div style="margin-top:37px;" class="marketPrice">$${value}<span style="color:#fff"> USD</span></div>`);
             }
